25b7e85090dfcbbbfc3d2728bc7f7c4d7e82146e
290274432@qq.com <+XSxs100206@>
2020-03-11 20:07:41 +0800

    spring Cloud

--- cloud-consumer-order80/src/main/java/com/sherlock/springcloud/MyIRuleConfig.java
+++ cloud-consumer-order80/src/main/java/com/sherlock/springcloud/MyIRuleConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 /**
  * @auther Sherlock
  * @date 2020/3/11 16:04
- * @Description:
+ * @Description: 包含重新定义负载均衡算法的bean不能在主配置可以扫描的包下（但是这个位置就是在可以扫描的包下啊，为什么测试是可以，算了，按照实际测试结果看）
  */
 @Configuration
 public class MyIRuleConfig {
--- cloud-consumer-order80/src/main/java/com/sherlock/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/sherlock/springcloud/OrderMain80.java
@@ -12,6 +12,9 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+/**
+ * value为服务提供方的名称spring.application.name,configuration为自定义包含负载均衡设置的bean
+  */
 @RibbonClient(value = "cloud-provider-payment", configuration = {MyIRuleConfig.class})
 public class OrderMain80 {
     public static void main(String[] args) {
